8fb5e3b36b10dfda0c0d4e344f8b5b18c4f4a732
victorious-firmware-2064 <alanna.nuala@gmail.com>
2019-10-21 22:06:40 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,8 @@
+let katzDeliLine = [];
+
+const takeANumber = (katzDeliLine, name) => {
+  if (name === true) {
+    katzDeliLine++
+    return `Welcome ${name}, you are number ${katzDeliLine} in line.` 
+  }
+}
